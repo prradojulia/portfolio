9c8edec8009b6e8dfb5d894fba727e53194036f9
julia <julia.prrado@gmail.com>
2026-08-10 23:32:55 -0300

    feat: adiciona responsividade para tablet e mobile

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 // ==============================
-// EmailJS
-// ==============================
-
-emailjs.init({
-    publicKey: "7X-qRe8RjxdGxCoRB",
-});
-
-// ==============================
 // Menu Mobile
 // ==============================
 
@@ -34,67 +26,4 @@ document.addEventListener("click", (e) => {
     if (!clicouNoMenu && !clicouNoHamburger) {
         menu.classList.remove("active");
     }
-});
-
-// ==============================
-// Formulário de Contato
-// ==============================
-
-const contatoForm = document.getElementById("contato-form");
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const mensagem = document.getElementById("mensagem");
-const botao = contatoForm.querySelector("button");
-
-// Mensagem de status
-const status = document.createElement("div");
-contatoForm.appendChild(status);
-
-contatoForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    if (
-        !nome.value.trim() ||
-        !email.value.trim() ||
-        !mensagem.value.trim()
-    ) {
-        status.textContent = "Preencha todos os campos.";
-        status.className = "error";
-        return;
-    }
-
-    botao.disabled = true;
-    botao.innerHTML = '<i class="fa-solid fa-spinner fa-spin"></i> Enviando...';
-
-    emailjs.send(
-        "service_ga8w51r",
-        "template_cpbblt9",
-        {
-            nome: nome.value.trim(),
-            email: email.value.trim(),
-            mensagem: mensagem.value.trim()
-        }
-    )
-    .then(() => {
-        status.textContent = "Mensagem enviada com sucesso!";
-        status.className = "success";
-
-        contatoForm.reset();
-    })
-    .catch((error) => {
-        status.textContent = "Erro ao enviar a mensagem.";
-        status.className = "error";
-
-        console.error("Erro EmailJS:", error);
-    })
-    .finally(() => {
-
-        botao.disabled = false;
-        botao.innerHTML = '<i class="fa-solid fa-paper-plane"></i> Enviar';
-
-        setTimeout(() => {
-            status.textContent = "";
-            status.className = "";
-        }, 5000);
-    });
 });
